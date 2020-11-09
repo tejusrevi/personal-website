b4c2f97765a6498137658d515537ef4c7ff5ffc2
Tejus <tejusrevi@gmail.com>
2020-11-09 10:44:24 -0500

    Added about page

---
+++ src/components/about-page/About.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+  return (
+    <div id="container">
+      <div id="profile-container">
+        <div id="profile-info">
+          <div id="profile-image"/>
+          <div id="content">
+            <h3>തേജസ് രവി</h3>
+            <h3>Tejus Revi</h3>
+            <hr/>
+            <div id="about-social">
+
+            <a id="linkedin" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/tejusrevi/">
+              <div id="linkedin-image" className="social-icon-about"/>
+            </a>
+    
+            <a id="github" target="_blank" rel="noopener noreferrer" href="https://github.com/tejusrevi">
+                <div id="github-image" className="social-icon-about"/>
+            </a>
+          </div>
+          </div>
+        </div>
+      </div>
+      <div id="details">
+
+        fdfgf
+        df
+        fdfgf
+        df
+        fdfgf
+        df
+        fdfgf
+        df
+        fdfgf
+        df
+        fdfgf
+        df
+        fdfgf
+        df
+        fdfgf
+        df
+        fdfgf
+        df
+      </div>
+    </div>
+  );
+  
+}
+
+export default About;
---
+++ src/components/projects-page/Projects.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//import './App.css';
+
+function Projects() {
+  return (
+    <div>
+        Project
+    </div>
+  );
+  
+}
+
+export default Projects;
